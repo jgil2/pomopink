4078371f81f9dea03b2b5c5ad5538a9d5aba25c9
jgil2 <jada.gilfillian@bison.howard.edu>
2026-04-18 13:35:31 -0400

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ashi.pomopink',
   appName: 'pomopink',
-  webDir: 'artifacts/mockup-sandbox/dist',
+  webDir: 'artifacts/pomodoro/dist',
   bundledWebRuntime: false
 };
 
